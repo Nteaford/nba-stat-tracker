458dd32d36370b8637565191deec03462b74451f
Nick Teaford <nickteaford@Nicks-iMac.local>
2021-12-17 13:24:32 -0800

    games API call finished

--- controllers/indexes.js
+++ controllers/indexes.js
@@ -15,8 +15,8 @@ async function onLoad(req, res) {
     // const nbaTeams = await onLoadNbaTeams();
     // const players = await onLoadPlayers();
     const games = await onLoadGames();
-    console.log(games);
     // const stats = await onLoadStats();
+    console.log(games);
     res.render('index', {title: "Welcome to NBA Stat Tracker", 
     // nbaTeams, 
     // players, 
@@ -25,45 +25,49 @@ async function onLoad(req, res) {
 });
 }
 
-//  async function onLoadNbaTeams(req, res) {
-//     const response = await fetch('https://www.balldontlie.io/api/v1/teams')
+
+// async function onLoadGames(req, res) {
+//     let finalProduct;
+//     const response = await fetch('https://www.balldontlie.io/api/v1/games?seasons[]=2021')
 //     .then(response => response.json())
-//     .then(function(nbaTeamData) {
-//         return nbaTeamData;
-//     })
+//     .then(function(gamesData) {
+//         console.log(gamesData);
+//         let dateRangeStart = moment().format();
+//         let dateRangeEnd = moment().subtract(5, 'days').format();
+//         let gamesDataFiltered = gamesData.data.filter(function(gamesDatum) {
+//             return (new Date(gamesDatum.date) <= new Date(dateRangeStart) && new Date(gamesDatum.date) >= new Date(dateRangeEnd));
+//         })
+//         console.log(gamesDataFiltered);
+//         finalProduct = gamesDataFiltered;
+//     });
+//     return finalProduct;
 
-    
 
-//     // .then (console.log(response));
-// }
-
-// function onLoadPlayers(req, res) {
-//     const response = fetch('https://www.balldontlie.io/api/v1/players')
+// async function onLoadStats(req, res) {
+//     let finalProduct;
+//     const response = await fetch('https://www.balldontlie.io/api/v1/stats')
 //     .then(response => response.json())
-//     .then (function(Data) {
-//         return
-//     }
-//     );
-// }
-async function onLoadGames(req, res) {
+//     .then(function(statsData) {
+//         console.log(statsData);
+//         let statsDataSorted = statsData.data.sort(function(statsDatumA, statsDatumB) {
+//             return (statsDatumA.id - statsDatumB.id);
+//         })
+//         console.log(statsDataSorted);
+//         finalProduct = statsDataSorted;
+//     });
+//     return finalProduct;
+
+    async function onLoadGames(req, res) {
     let finalProduct;
     const response = await fetch('https://www.balldontlie.io/api/v1/games?seasons[]=2021')
     .then(response => response.json())
     .then(function(gamesData) {
-        console.log(gamesData.data);
-        let dateRangeStart = moment().format();
-        let dateRangeEnd = moment().subtract(5, 'days').format();
-        let gamesDataFiltered = gamesData.data.filter(function(gamesDatum) {
-            return (new Date(gamesDatum.date) <= new Date(dateRangeStart) && new Date(gamesDatum.date) >= new Date(dateRangeEnd));
+        let gamesDataSorted = gamesData.data.sort(function(gamesDatumA, gamesDatumB) {
+            return (gamesDatumA.id - gamesDatumB.id);
         })
-        console.log(gamesDataFiltered);
-        finalProduct = gamesDataFiltered;
+        let gameSlice = gamesDataSorted.slice((gamesDataSorted.length-5), gamesDataSorted.length)
+        finalProduct = gameSlice;
     });
     return finalProduct;
 }
-// function onLoadStats(req, res) {
-//     const response = fetch('https://www.balldontlie.io/api/v1/stats?seasons[]=2021')
-//     .then(response => response.json())
-//     .then (function(Data)
-//     );
-// }
+
