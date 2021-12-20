bab2594ae425782cfcb61b6235fb96d9f5fefc98
Nick Teaford <nickteaford@Nicks-iMac.local>
2021-12-20 15:33:04 -0800

    player selection on new page mock up

--- controllers/teams.js
+++ controllers/teams.js
@@ -3,6 +3,9 @@ const User = require('../models/user')
 const fetch = require('node-fetch');
 const moment = require('moment');
 
+const playersURL = 'https://www.balldontlie.io/api/v1/players';
+const teamsURL = 'https://www.balldontlie.io/api/v1/teams?seasons[]=2021';
+
 module.exports = {
 index,
 new: newTeam,
@@ -19,10 +22,10 @@ async function index(req, res) {
 
 
 async function newTeam(req, res) {
-    const teams = req.user.teams;
-
+   const teams = await onLoadTeams();
+console.log(teams);
     //  teams = await User.teams.find({});
-    res.render("teams/new", { title: "Pick your Teams"});
+    res.render("teams/new", { title: "NBA Draft Day", teams});
 }
 
 
@@ -38,4 +41,29 @@ async function create(req, res) {
 
 
 
+async function onLoadTeams(req, res) {
+    let finalProduct;
+    const response = await fetch(teamsURL)
+    .then(response => response.json())
+    .then(function (teamsData) {
+        
+            finalProduct = teamsData;
+        });
+    return finalProduct;
+}
 
+async function teamToPlayerPG(req, res) {
+    
+    let playerId;
+    let endProduct = [];
+    
+    const response = await fetch(`${playersURL}`)
+    .then(response => response.json())
+    .then(function (playerData) {
+        let playerFilter = playerData.data.filter(function (playerDatum) {
+            return (playerDatum.team.id === req.body && playerDatum.position === "PG");
+            })
+            endProduct = playerFilter;
+        })
+        return endProduct;  
+    }
