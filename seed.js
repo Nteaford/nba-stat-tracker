8b63705d4b16dfa3811ae8d683064b7b8581a02c
Nick Teaford <nickteaford@Nicks-iMac.local>
2021-12-22 20:10:49 -0800

    seed file finalized and run

--- seed.js
+++ seed.js
@@ -1,30 +1,17 @@
 require('dotenv').config();
 require('./config/database');
-const NBATeam = require('./models/nbaTeam')
 const Player = require('./models/player')
+const Team = require('./models/team')
 const data = require('./data')
 
-const p1 = NBATeam.deleteMany({});
-const p2 = Player.deleteMany({});
+const p1 = Player.deleteMany({});
+const p2 = Team.deleteMany({});
 
 Promise.all([p1, p2])
 .then(function(results) {
   console.log(results);
-  return Performer.create(data.performers);
-}).then(function(performers) {
-  console.log(performers);
-  return Movie.create(data.movies);
-}).then(function(movies) {
-  console.log(movies);
-  return Promise.all([
-    Performer.findOne({ name: "Mark Hamill" }),
-    Movie.findOne({ title: "Star Wars - A New Hope" }),
-  ]);
-}).then(function(results) {
-  const mark = results[0];
-  const starWars = results[1];
-  starWars.cast.push(mark);
-  return starWars.save();
-}).then(function() {
+  return Player.create(data.players);
+}).then(function(players) {
+  console.log(players);
   process.exit();
 });
